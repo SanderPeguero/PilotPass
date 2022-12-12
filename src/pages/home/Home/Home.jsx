08c490f4074af8e08ea754cfbe054df14c7248b6
jose22gonzalez <97250949+jose22gonzalez@users.noreply.github.com>
2022-12-12 17:33:52 -0400

    Adding the Sign Up button and removing the Alert button

--- src/pages/home/Home/Home.jsx
+++ src/pages/home/Home/Home.jsx
@@ -12,6 +12,7 @@ const HomeBox = () => {
                 <li><a href="#">Home</a></li>
                 <li><a href="#/plan">Plan</a></li>
                 <li><a href="#/login">Login</a></li>
+                <li><a href="#/signup">Sign Up</a></li>
             </ul>
 
         </>
