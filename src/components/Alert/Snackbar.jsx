93357abe405de92fd33795de466048e952e6c897
Sander Peguero <sanderpeguero.21@gmail.com>
2022-12-13 05:30:28 -0400

    Redux Class to Functional Component / Error Alerts

--- src/components/Alert/Snackbar.jsx
+++ src/components/Alert/Snackbar.jsx
@@ -60,9 +60,9 @@ const SnackbarAlert = ({ severity, title, detail }) => {
 
     return (
         <Stack spacing={2} sx={{ width: '100%' }}>
-            <Button variant="outlined" onClick={handleClick}>
+            {/* <Button variant="outlined" onClick={handleClick}>
                 Open success snackbar
-            </Button>
+            </Button> */}
             <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
                 {/* <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
                 This is a success message!
--- src/main.jsx
+++ src/main.jsx
@@ -2,26 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import App from './App'
-//Redux
-import { createStore, compose, applyMiddleware } from 'redux'
-import { Provider } from 'react-redux'
-import thunk from 'redux-thunk'
-//Styles
 import './index.css'
 
-import rootReducer from './store/reducers/rootReducer'
-// import reportWebVitals from './reportWebVitals'
-
-//devtools
-const composeEnhancers = typeof window == 'object' &&
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
-  : compose;
-
-  const store = createStore(
-    rootReducer,
-    composeEnhancers(applyMiddleware(thunk))
-  )
+//Redux
+import store from './redux/store'
+import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
