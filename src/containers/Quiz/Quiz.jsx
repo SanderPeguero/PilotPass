07d3a3eb992c0714e2b4d5b8b1d173e18d62d2d8
Sander Peguero <sanderpeguero.21@gmail.com>
2022-12-31 03:41:45 -0400

    Css Format 2

--- src/containers/Quiz/Quiz.jsx
+++ src/containers/Quiz/Quiz.jsx
@@ -66,7 +66,7 @@ const Quiz = (props) => {
 
     return (
         <Box className={classes.Quiz}>
-            <div className="row cards col-lg-4 col-md-7 col-sm-12">
+            <div className="row cards col-lg-4 col-md-7 col-sm-12" style={{ paddingTop: '2rem' , paddingBottom: '100px'}}>
                 <div className="card-header col-lg-4 col-md-7 col-sm-12" style={{color:'white', margin: "2%"}}>
                     <h2 className="col-lg-4 col-md-7 col-sm-12">Test: {name()}</h2>
                 </div>
