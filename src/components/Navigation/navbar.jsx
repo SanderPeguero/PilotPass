3586387955b9e4b6f267190924b7c3855755a30a
Sander Peguero <sanderpeguero.21@gmail.com>
2023-01-25 03:59:57 -0400

    Less Firebase Calls

--- src/components/Navigation/navbar.jsx
+++ src/components/Navigation/navbar.jsx
@@ -32,12 +32,12 @@ const Navbar = ({ isAuthenticated }) => {
                     </NavLink>
                 </li>
 
-                <li className="nav-item">
+                {/* <li className="nav-item">
                     <NavLink to="/quiz-creator" className="nav-link">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" className="fa-primary" d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zM200 344V280H136c-13.3 0-24-10.7-24-24s10.7-24 24-24h64V168c0-13.3 10.7-24 24-24s24 10.7 24 24v64h64c13.3 0 24 10.7 24 24s-10.7 24-24 24H248v64c0 13.3-10.7 24-24 24s-24-10.7-24-24z"/></svg>
                         <span className="link-text">Create</span>
                     </NavLink>
-                </li>
+                </li> */}
 
                 {/* Log Out */}
                 <li className="nav-item">
